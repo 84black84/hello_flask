d068258571bf3febdae67517358ac0411cef66cf
Mavrogiannis, Christos <Christos.Mavrogiannis@ottobock.de>
2020-12-29 16:27:36 +0100

    introducting the DebugToolbarExtension

--- hello_flask_app/__init__.py
+++ hello_flask_app/__init__.py
@@ -1,2 +1,7 @@
 import flask
+from flask_debugtoolbar import DebugToolbarExtension
+
 app = flask.Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = '7a23ac9c-985f-41fc-a378-e8080468174c'
+toolbar = DebugToolbarExtension(app)
